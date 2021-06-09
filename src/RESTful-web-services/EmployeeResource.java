df4474dd26ec7b65e548d07b4aac73a48ce7f884
Tarun Telang <tarun.telang@gmail.com>
2021-06-09 10:12:41 +0530

    Update EmployeeResource.java

--- src/RESTful-web-services/EmployeeResource.java
+++ src/RESTful-web-services/EmployeeResource.java
@@ -1,9 +1,8 @@
 Path("/employee")
 public class EmployeeResource  {
 	@GET
-	@Path(“/get/{id}”)
-	@Produces(
-		MediaTypes.APPLICATION_JSON)
+	@Path("/get/{id}")
+	@Produces(MediaTypes.APPLICATION_JSON)
 	public Response getEmployee(@PathParam("id") int id) {
  		return Response.ok().entity(new Employee(id, "james", "james@com")).build();
 	}
